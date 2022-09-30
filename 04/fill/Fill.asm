95b15e2c5bc8411e0148d7ca2a15522f200a72e8
Sang Nguyen <nguysa02@risk.regn.net>
2022-09-30 14:53:53 +0700

    fill.asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,53 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+@24576 // 0
+D=A //
+@R0 //
+M=D // save size of the screen
+@KBD //4
+D=M // get keyboard value
+@10 
+D;JNE // if keyboard is being pressed jump to instruction 10
+@30
+0;JMP // else jump to instruction 4
+@SCREEN // 10
+D=A //
+@R1 //
+M=D // reset current pixel position
+@SCREEN
+D=M
+@4
+D+1; JEQ
+@R0 // 24
+D=M // get screen size
+@R1
+M=M+1
+A=M-1
+M=-1 // set screen to black
+@R1
+D=D-M
+@18
+D;JNE
+@4
+0;JMP
+@SCREEN
+D=M
+@4
+D; JEQ
+@SCREEN // 10
+D=A //
+@R1 //
+M=D // reset current pixel position
+@R0 //38
+D=M // get screen size
+@R1
+M=M+1
+A=M-1
+M=0 // set screen to white
+@R1
+D=D-M
+@38
+D;JNE
+@4
+0;JMP
